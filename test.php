25a8fb06cf8f08423f4006c58dd9ef609bcb2722
zhuwpeng <zhuwpeng@gmail.com>
2016-04-14 17:58:54 +0200

    Added function to create select option
    -Created function that generates select options based on what the user
    wants.

--- test.php
+++ test.php
@@ -1,5 +1,9 @@
 <?php
-include "db_connect.php";
+include "inc/db_connect.php";
+error_reporting(E_ALL & E_STRICT);
+ini_set('display_errors', '1');
+ini_set('log_errors', '0');
+ini_set('error_log', './');
 
 $tussen = "'t";
 $addslash = addslashes($tussen);
@@ -8,3 +12,26 @@ $esc = mysqli_real_escape_string($connect, $tussen);
 
 
 echo $tussen . " " . $esc;
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+<?php 
+$sports = array('tennis', 'voetbal', 'tafeltennis', 'biljart');
+$sportSelectName = "sportonderdeel";
+$sportSelectId = "form-sport";
+
+echo '<select name="'. $sportSelectName . '" id="' . $sportSelectId . '">';
+foreach($sports as $option) {
+	echo '<option value="' . strtolower($option) . '">' . ucfirst(strtolower($option)) . '</option>';
+}
+echo '</select>';
+
+?>
+</body>
+</html>
