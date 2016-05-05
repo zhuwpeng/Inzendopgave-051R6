2618a5c5cf230307c066a284252db650835e6599
zhuwpeng <zhuwpeng@gmail.com>
2016-05-05 17:56:08 +0200

    Tried to use loops
    Tried to use a foreach loop to loop through every user input in the post
    array. Error messages are not working yet.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include "inc/db_connect.php";
 include "inc/functions.php";
-set_error_handler("error_msg");
+//set_error_handler("error_msg");
 
 $naamError = "";
 $anaamError = "";
@@ -17,63 +17,97 @@ $geslachtError = "";
 
 $message = "";
 $errorMsg = "";
-$errors = array('form'=>array('gNaam'=>"U heeft geen naam ingevuld",
-							'gAnaam'=>"U heeft geen achternaam ingevuld",
-							'gHuisnr'=>"U heeft geen huisnummer ingevuld",
-							'gEmail'=>"U heeft geen e-mail ingevuld",
-							'gDatum'=>"U heeft geen datum opgegeven",
-							'gGeslacht'=>"Kies een geslacht",
-							'ongStraat'=>"Straatnaam mag alleen bestaan uit letters",
-							'ongEmail'=>"E-mail moet 2 op de volgende manier '2 letters'@'2 letters' en eindigen op '.nl'(vb. ab@cd.nl)",
-							'ongPostcode'=>"Postcode moet bestaan uit 4 cijfers en 2 letters (vb. 1234ab)",
-							'ongHuisnr'=>"Huisnummer moet bestaan uit alleen nummers",
-							'ongToevging'=>"Toevoeging mag alleen bestaan uit letters",
-							'ongWoonplts'=>"Woonplaats mag alleen bestaan uit letters",
-							'ongDatum'=>"Datum format moet dd-mm-yyyy zijn (vb. 23-01-2000)",
-							'toekDatum'=>"Datum ligt in de toekomst")
+$errors = array('geen'=>array('naam'=>"U heeft geen naam ingevuld",
+							'achternaam'=>"U heeft geen achternaam ingevuld",
+							'email'=>"U heeft geen e-mail ingevuld",
+							'datum'=>"U heeft geen datum opgegeven",
+							'geslacht'=>"Kies een geslacht"),
+			'ongeldig'=>array('straat'=>"Straatnaam mag alleen bestaan uit letters",
+							'email'=>"E-mail moet 2 op de volgende manier '2 letters'@'2 letters' en eindigen op '.nl'(vb. ab@cd.nl)",
+							'postcode'=>"Postcode moet bestaan uit 4 cijfers en 2 letters (vb. 1234ab)",
+							'huisnummer'=>"Huisnummer moet bestaan uit alleen nummers",
+							'toevoeging'=>"Toevoeging mag alleen bestaan uit letters",
+							'woonplaats'=>"Woonplaats mag alleen bestaan uit letters",
+							'datum'=>"Datum format moet dd-mm-yyyy zijn (vb. 23-01-2000)",
+							'gebdatum'=>"Datum ligt in de toekomst")
 				);
 
 $error = false;
 
 //check of submit isset
 if(isset($_POST['submit']) && $_POST['submit'] == 'Verstuur'){
+	
+	foreach ($_POST as $key => $input) {
+		if ($key != "submit") {
+			$stripTrim[$key]= stripslashes(trim($input));
 			
-	$naam = stripslashes(trim($_POST['naam']));
-	$tussenvoegsel = stripslashes(trim(isset($_POST['tussenvoegsel'])?$_POST['tussenvoegsel']:NULL));
-	$achternaam = stripslashes(trim($_POST['achternaam']));
-	$gebDatum = stripslashes(trim(isset($_POST['gebdatum'])?$_POST['gebdatum']:NULL));
-	$straat = stripslashes(trim(isset($_POST['straat'])?$_POST['straat']:NULL));
-	$postcode = stripslashes(trim(isset($_POST['postcode'])?$_POST['postcode']:NULL));
-	$huisnummer = stripslashes(trim(isset($_POST['huisnummer'])?$_POST['huisnummer']:NULL));
-	$toevoeging = stripslashes(trim(isset($_POST['toevoeging'])?$_POST['toevoeging']:NULL));
-	$woonplaats = stripslashes(trim(isset($_POST['woonplaats'])?$_POST['woonplaats']:NULL));
-	$email = stripslashes(trim(isset($_POST['email'])?$_POST['email']:NULL));
-	$ingDatum = stripslashes(trim($_POST['ingdatum']));
-	$geslacht = stripslashes(trim((isset($_POST['geslacht'])?$_POST['geslacht']:NULL)));
+			if (empty($input)) {
+				if ($key == "tussenvoegsel" || 
+					$key == "gebdatum" || 
+					$key == "straat" ||
+					$key == "postcode" ||
+					$key == "huisnummer" ||
+					$key == "toevoeging" ||
+					$key == "woonplaats" ||
+					$key == "ingdatum") {
+					$stripTrim[$key] = NULL;
+				} else {
+					$errorMsges[$key] = $errors['geen'][$key];
+				}
+			} else {
+				$errorMsges[$key] = "";
+			}
+		}
+	}
+	
+	$naam = $stripTrim['naam'];
+	$tussenvoegsel = $stripTrim['tussenvoegsel'];
+	$achternaam = $stripTrim['achternaam'];
+	$gebDatum = $stripTrim['gebdatum'];
+	$straat = $stripTrim['straat'];
+	$postcode = $stripTrim['postcode'];
+	$huisnummer = $stripTrim['huisnummer'];
+	$toevoeging = $stripTrim['toevoeging'];
+	$woonplaats = $stripTrim['woonplaats'];
+	$email = $stripTrim['email'];
+	$ingDatum = $stripTrim['ingdatum'];
+	$geslacht = $stripTrim['geslacht'];
+	
+// 	$naam = stripslashes(trim($_POST['naam']));
+// 	$tussenvoegsel = stripslashes(trim(isset($_POST['tussenvoegsel'])?$_POST['tussenvoegsel']:NULL));
+// 	$achternaam = stripslashes(trim($_POST['achternaam']));
+// 	$gebDatum = stripslashes(trim(isset($_POST['gebdatum'])?$_POST['gebdatum']:NULL));
+// 	$straat = stripslashes(trim(isset($_POST['straat'])?$_POST['straat']:NULL));
+// 	$postcode = stripslashes(trim(isset($_POST['postcode'])?$_POST['postcode']:NULL));
+// 	$huisnummer = stripslashes(trim(isset($_POST['huisnummer'])?$_POST['huisnummer']:NULL));
+// 	$toevoeging = stripslashes(trim(isset($_POST['toevoeging'])?$_POST['toevoeging']:NULL));
+// 	$woonplaats = stripslashes(trim(isset($_POST['woonplaats'])?$_POST['woonplaats']:NULL));
+// 	$email = stripslashes(trim(isset($_POST['email'])?$_POST['email']:NULL));
+// 	$ingDatum = stripslashes(trim($_POST['ingdatum']));
+// 	$geslacht = stripslashes(trim((isset($_POST['geslacht'])?$_POST['geslacht']:NULL)));
 	
 	//Valideer naam
-	if(empty($naam)){
-		$naamError = $errors['form']['gNaam'];
-		$error = true;
-	}
+// 	if(empty($naam)){
+// 		$naamError = $errors['geen']['naam'];
+// 		$error = true;
+// 	}
 	
 	//Valideer achternaam
-	if(empty($achternaam)){
-		$anaamError = $errors['form']['gAnaam'];
-		$error = true;
-	}
+// 	if(empty($achternaam)){
+// 		$anaamError = $errors['geen']['achternaam'];
+// 		$error = true;
+// 	}
 	
 	//Valideer geboorte datum
 	if(!empty($gebDatum)){
 		$valgebDatum = date_validation($gebDatum);
 	
 		if($valgebDatum == false){
-			$gebdateError = $errors['form']['ongDatum'];
-			$error = true;
+			$errorMsges = $errors['ongeldig']['datum'];
 		}
 	
 		if(strtotime($valgebDatum)>strtotime(date("Y-m-d"))){
-			$gebdateError = $errors['form']['toekDatum'];
+			$errorMsges = $errors['ongeldig']['gebdatum'];
 			$error = true;
 		}
 	}else{
@@ -83,7 +117,7 @@ if(isset($_POST['submit']) && $_POST['submit'] == 'Verstuur'){
 	//Valideer straat
 	if(!empty($straat)){
 		if(!preg_match('/^[a-zA-Z]*$/', $straat)){
-			$huisnrError = $errors['form']['ongStraat'];
+			$errorMsges = $errors['ongeldig']['straat'];
 			$error = true;
 		}
 	}
@@ -91,7 +125,7 @@ if(isset($_POST['submit']) && $_POST['submit'] == 'Verstuur'){
 	//Valideer huisnummer
 	if(!empty($huisnummer)){
 		if(!preg_match('/^[0-9]*$/', $huisnummer)){
-			$huisnrError = $errors['form']['ongHuisnr'];
+			$errorMsges = $errors['ongeldig']['huisnummer'];
 			$error = true;
 		}
 	}
@@ -99,7 +133,7 @@ if(isset($_POST['submit']) && $_POST['submit'] == 'Verstuur'){
 	//Valideer toevoeging
 	if(!empty($toevoeging)) {
 		if(!preg_match('/^[a-zA-Z]*$/', $toevoeging)) {
-			$toevgingError = $errors['form']['ongToevging'];
+			$errorMsges = $errors['ongeldig']['toevoeging'];
 			$error = true;
 		}
 	}
@@ -107,7 +141,7 @@ if(isset($_POST['submit']) && $_POST['submit'] == 'Verstuur'){
 	//Valideer postcode
 	if(!empty($postcode)){
 		if(!preg_match('/^(\d\d\d\d)[a-zA-Z][a-zA-Z]$/', $postcode)){
-			$postcodeError = $errors['form']['ongPostcode'];
+			$errorMsges = $errors['ongeldig']['postcode'];
 			$error = true;
 		}
 	}
@@ -115,44 +149,39 @@ if(isset($_POST['submit']) && $_POST['submit'] == 'Verstuur'){
 	//Valideer woonplaats
 	if(!empty($woonplaats)){
 		if(!preg_match('/^[a-zA-Z]+$/', $woonplaats)){
-		$woonpltsError = $errors['form']['ongWoonplts'];
+		$errorMsges = $errors['ongeldig']['woonplaats'];
 		$error = true;
 		}
 	}
 	
 	//Valideer email
 	if(empty($email)){
-		$emailError = $errors['form']['gEmail'];
+		$errorMsges = $errors['geen']['email'];
 		$error = true;
 	}else{
 		$checkEmail = test_email($email);
 		if($checkEmail=="ongeldig"){
-			$emailError = $errors['form']['ongEmail'];
+			$errorMsges = $errors['ongeldig']['email'];
 			$error = true;
 		}
 	}
 	
 	//Valideer ingangsdatum
 	if(empty($ingDatum)){
-		$ingdateError = $errors['form']['gDatum'];
+		$errorMsges = $errors['geen']['datum'];
 		$error = true;
 	}else{
 		$valIngDatum = date_validation($ingDatum);
 		
 		if($valIngDatum == false){
-			$ingdateError = $errors['form']['ongDatum'];
-			$error = true;
-		}
-		
-		if($valIngDatum == "Toekomst"){
-			$ingdateError = $errors['form']['toekDatum'];
+			$errorMsges = $errors['ongeldig']['datum'];
 			$error = true;
 		}
 	}
 	
 	//Valideer geslacht
 	if(empty($geslacht)){
-		$geslachtError = $errors['form']['gGeslacht'];
+		$errorMsges = $errors['geen']['geslacht'];
 		$error = true;
 	}
 	
@@ -187,16 +216,16 @@ if(isset($_POST['submit']) && $_POST['submit'] == 'Verstuur'){
 											Geboortedatum, 
 											Geslacht)
 									VALUES (NULL, 
-											'$naamEsc',
-											'$tussenvoegsel',
-											'$achternaamEsc',
-											'$straatEsc',
-											'$huisToev',
-											'$postcodeEsc',
-											'$woonplaatsEsc',
-											'$emailEsc',
-											'$gebdatEsc',
-											'$geslachtEsc')";
+											'$naamEsc',"
+											. ($tussenvoegsel==NULL ? "NULL" : "'$tussen'") .",
+											'$achternaamEsc',"
+											. ($straatEsc==NULL ? "NULL" : "'$straatEsc'") .","
+											. ($huisnummerEsc==NULL ? "NULL" : "'$huisnummerEsc'") .","
+											. ($postcodeEsc==NULL ? "NULL" : "'$postcodeEsc'") .","
+											. ($woonplaatsEsc==NULL ? "NULL" : "'$woonplaatsEsc'").",
+											'$emailEsc',"
+											. ($gebdatEsc==NULL ? "NULL" : "'$gebdatEsc'") . ",
+											'$geslachtEsc');";
 		
 		//Retrieve latest person id
 		$ledenResult = mysqli_query($connect, $ledenQuery) or die( "Er is iets mis gegeaan tijdens het invoeren van gegevens." . " " . mysqli_error($connect).".");
@@ -288,7 +317,7 @@ if(isset($_POST['reset']) && $_POST['reset'] == "Reset"){
 <h1>Omnisportvereniging</h1>
 </header>
 <div id="container">
-		<span><?php echo $message;?></span>
+		<h2 class="message"><?php echo $message;?></h2>
 		<span class=error><?php echo $errorMsg;?></span>
 		<div class="imgtext">
 		    <img alt="sport" src="img/sports.jpg">
@@ -300,60 +329,60 @@ if(isset($_POST['reset']) && $_POST['reset'] == "Reset"){
 	        <h2>Aanmeld formulier</h2>
 	        <form method="POST" action="index.php">
 				<div class="left-side">
-		            <span class = error><?php echo $naamError;?></span>
+		            <span class = error><?php if(isset($errorMsges['naam'])){echo $errorMsges['naam'];}?></span>
 		            <label for="form-voornaam">*Naam:</label>
-		            <input type="text" id="form-voornaam" name="naam" value="<?php if($error){echo htmlentities($_POST['naam']);}else{ echo "";}?>">
+		            <input type="text" id="form-voornaam" name="naam" value="<?php if(isset($_POST['naam'])){ echo htmlentities($_POST['naam']);}else{ echo "";}?>">
 		
 		            <label for="form-tussenvoegsel">Tussenvoegsel:</label>
-		            <input type="text" id="form-tussenvoegsel" name="tussenvoegsel" value="<?php if($error){echo htmlentities($_POST['tussenvoegsel']);}else{ echo "";}?>">
+		            <input type="text" id="form-tussenvoegsel" name="tussenvoegsel" value="<?php if(isset($_POST['tussenvoegsel'])){ echo htmlentities($_POST['tussenvoegsel']);}else{ echo "";}?>">
 		
-		            <span class = error><?php echo $anaamError;?></span>
+		            <span class = error><?php if(isset($errorMsges['achternaam'])){echo $errorMsges['achternaam'];}?></span>
 		            <label for="form-achternaam">*Achternaam:</label>
-		            <input type="text" id="form-achternaam" name="achternaam" value="<?php if($error){echo htmlentities($_POST['achternaam']);}else{ echo "";}?>">
+		            <input type="text" id="form-achternaam" name="achternaam" value="<?php if(isset($_POST['achternaam'])){ echo htmlentities($_POST['achternaam']);}else{ echo "";}?>">
 					
-					<span class = error><?php echo $gebdateError;?></span>
+					<span class = error><?php if(isset($errorMsges['gebdatum'])){echo $errorMsges['gebdatum'];}?></span>
 		            <label for="form-gebdatum">Geboortedatum</label>
-		            <input type="text" id="form-gebdatum" name="gebdatum" value="<?php if($error){echo htmlentities($_POST['gebdatum']);}else{ echo "";}?>">
+		            <input type="text" id="form-gebdatum" name="gebdatum" value="<?php if(isset($_POST['gebdatum'])){ echo htmlentities($_POST['gebdatum']);}else{ echo "";}?>">
 					
 		            <div id = "straat-huisnr">
-				            <span class = error><?php echo $straatError;?></span>
-				            <span class = error><?php echo $huisnrError;?></span>
-				            <span class = error><?php echo $toevgingError;?></span>
+				            <span class = error><?php if(isset($errorMsges['straat'])){echo $errorMsges['straat'];}?></span>
+				            <span class = error><?php if(isset($errorMsges['huisnummer'])){echo $errorMsges['huisnummer'];}?></span>
+				            <span class = error><?php if(isset($errorMsges['toevoeging'])){echo $errorMsges['toevoeging'];}?></span>
 		                    <div class = "straat">
 	                            <label for="form-straat">Straat:</label>
-	                            <input type="text" id="form-straat" name="straat" value="<?php if($error){echo htmlentities($_POST['straat']);}else{ echo "";}?>">
+	                            <input type="text" id="form-straat" name="straat" value="<?php if(isset($_POST['straat'])){ echo htmlentities($_POST['straat']);}else{ echo "";}?>">
 		                    </div>
 		                    <div class = "huisnr">
 	                            <label for="form-huisnummer">nr.:</label>
-	                            <input type="number" max="9999" id="form-huisnummer" name="huisnummer" value="<?php if($error){echo htmlentities($_POST['huisnummer']);}else{ echo "";}?>">
+	                            <input type="number" max="9999" id="form-huisnummer" name="huisnummer" value="<?php if(isset($_POST['huisnummer'])){ echo htmlentities($_POST['huisnummer']);}else{ echo "";}?>">
 		                    </div>
 		                    <div class = "toevoeging">
 		                    	<label for="form-toevoeging">Toev.</label>
-		                    	<input type="text" id="form-toevoeging" maxlength="3" name="toevoeging" value="<?php if($error){echo htmlentities($_POST['toevoeging']);}else{ echo "";}?>">
+		                    	<input type="text" id="form-toevoeging" maxlength="3" name="toevoeging" value="<?php if(isset($_POST['toevoeging'])){ echo htmlentities($_POST['toevoeging']);}else{ echo "";}?>">
 		                    </div>
 		            </div>
 		            
-                    <span class = error><?php echo $postcodeError;?></span>
+                    <span class = error><?php if(isset($errorMsges['postcode'])){echo $errorMsges['postcode'];}?></span>
                     <label for="form-postcode">Postcode:</label>
-                    <input type="text" id="form-postcode" name="postcode" value="<?php if($error){echo htmlentities($_POST['postcode']);}else{ echo "";}?>">
+                    <input type="text" id="form-postcode" name="postcode" value="<?php if(isset($_POST['postcode'])){ echo htmlentities($_POST['postcode']);}else{ echo "";}?>">
 				</div>
 				
 				<div class="right-side">
-		            <span class = error><?php echo $woonpltsError;?></span>
+		            <span class = error><?php if(isset($errorMsges['woonplaats'])){echo $errorMsges['woonplaats'];}?></span>
 		            <label for="form-woonplaats">Woonplaats:</label>
-		            <input type="text" id="form-woonplaats" name="woonplaats" value="<?php if($error){echo htmlentities($_POST['woonplaats']);}else{ echo "";}?>">
+		            <input type="text" id="form-woonplaats" name="woonplaats" value="<?php if(isset($_POST['woonplaats'])){ echo htmlentities($_POST['woonplaats']);}else{ echo "";}?>">
 		
-		            <span class = error><?php echo $emailError;?></span>
+		            <span class = error><?php if(isset($errorMsges['email'])){echo $errorMsges['email'];}?></span>
 		            <label for="form-email">*E-mail:</label>
-		            <input type="email" id="form-email" name="email" value="<?php if($error){echo htmlentities($_POST['email']);}else{ echo "";}?>">
+		            <input type="email" id="form-email" name="email" value="<?php if(isset($_POST['email'])){ echo htmlentities($_POST['email']);}else{ echo "";}?>">
 		
-		            <span class = error><?php echo $ingdateError;?></span>
+		            <span class = error><?php if(isset($errorMsges['ingdatum'])){echo $errorMsges['ingdatum'];}?></span>
 		            <label for="form-ingdatum">*Ingangsdatum</label>
-		            <input type="text" id="form-ingdatum" name="ingdatum" value="<?php if($error){echo htmlentities($_POST['ingdatum']);}else{ echo "";}?>">
+		            <input type="text" id="form-ingdatum" name="ingdatum" value="<?php if(isset($_POST['ingdatum'])){ echo htmlentities($_POST['ingdatum']);}else{ echo "";}?>">
 		
 		            <div class="geslacht">
 		                    <label class="geslacht-title">Geslacht</label><span class = error><?php echo $geslachtError;?></span>
-		                    <input type="radio" id="form-man" name="geslacht" <?php if(isset($_POST['geslacht']) && $_POST['geslacht']=='m'){ echo ' checked="checked" ';}?> value="m"><label for="form-man">Man</label>
+		                    <input type="radio" id="form-man" name="geslacht" checked="checked" <?php if(isset($_POST['geslacht']) && $_POST['geslacht']=='m'){ echo ' checked="checked" ';}?> value="m"><label for="form-man">Man</label>
 		                    <input type="radio" id="form-vrouw" name="geslacht" <?php if(isset($_POST['geslacht']) && $_POST['geslacht']=='v'){ echo ' checked="checked" ';}?> value="v"><label for="form-vrouw">Vrouw</label>
 		            </div>
 		
